b8470ab7ea9604cb4aea6ec1172929e03743dc2d
Kyle Kowalski <Kyle.Kowalski@gmail.com>
2017-10-18 12:31:04 -0600

    everything saves (locally) - seems to be intact - now for heroku

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,8 +13,7 @@ module.exports = function(app){
         // Get our new friend
         let returnFriend = friendLogic.findOurFriend(newFriend);
         // Store this friend so we have more data points (might as well?  How will this work up on heroku?);  
-        // TODO RE-ENABLE THIS (once testing is completed);
-        // friendLogic.writeFriendToFile(newFriend);
+        friendLogic.writeFriendToFile(newFriend);
 
         res.json(returnFriend);
         res.end();
